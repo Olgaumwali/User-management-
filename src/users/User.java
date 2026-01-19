77c79460db191a5fc1c617cd34014f9710269435
Olgaumwali <olgaumwali19@gmail.com>
2026-01-19 13:34:29 +0200

    Add User class with login and feature access methods

---
+++ src/users/User.java
@@ -0,0 +1,21 @@
+package users;
+
+public class User {
+    protected int userId;
+    protected String name;
+    protected String email;
+
+    public User(int userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void login() {
+        System.out.println(name + " logged in successfully.");
+    }
+
+    public void accessFeatures() {
+        System.out.println("Accessing general user features.");
+    }
+}
